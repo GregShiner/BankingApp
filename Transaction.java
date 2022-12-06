faa6ec4179fce79486ff48c1cc928434ad8f2602
GregShiner <greg@justifinetwork.com>
2022-12-06 18:14:42 -0500

    Create transaction class

---
+++ Transaction.java
@@ -0,0 +1,10 @@
+public class Transaction {
+    public String description;
+    public String date;
+    public double amount;
+    public Transaction (String description, String date, double amount) {
+        this.description = description;
+        this.date = date;
+        this.amount = amount;
+    }
+}
