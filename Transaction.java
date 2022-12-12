355381b55a3338612528b1c6d1e4addf825532cb
GregShiner <greg@justifinetwork.com>
2022-12-12 01:14:56 -0500

    Add toStringArray to Transaction class

--- Transaction.java
+++ Transaction.java
@@ -7,4 +7,12 @@ public class Transaction {
         this.date = date;
         this.amount = amount;
     }
+    public String[] toStringArray() {
+        String[] result = new String[4];
+        result[0] = "transaction:";
+        result[1] = "description:" + description;
+        result[2] = "date:" + date;
+        result[3] = "amount:" + amount;
+        return result;
+    }
 }
