7db7c89cfb29b71c2a9c99d02346b792cc375f9b
GregShiner <greg@justifinetwork.com>
2022-12-12 17:38:01 -0500

    Fix spacing on transaction strings

--- Transaction.java
+++ Transaction.java
@@ -10,9 +10,9 @@ public class Transaction {
     public String[] toStringArray() {
         String[] result = new String[4];
         result[0] = "transaction:";
-        result[1] = "description:" + description;
-        result[2] = "date:" + date;
-        result[3] = "amount:" + amount;
+        result[1] = "description: " + description;
+        result[2] = "date: " + date;
+        result[3] = "amount: " + amount;
         return result;
     }
 }
